ad2c3c4b309a39e557452cb74b57194745203f67
Callum Connolly <callumc11@gmail.com>
2020-04-12 12:33:02 +0100

    Initial Commit with sample project to test in different environments

---
+++ src/main/java/cc14g17/App.java
@@ -0,0 +1,23 @@
+package cc14g17;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        if (args.length < 1) {
+            System.err.println("Please provide an argument");
+            System.exit(0);
+        }
+        System.out.println(sha256hex(args[0]));
+    }
+
+    static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
+    }
+}
