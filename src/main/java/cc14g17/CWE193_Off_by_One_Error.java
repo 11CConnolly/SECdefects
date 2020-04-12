aba76a820715ae1df0377f5794adf19b52dac945
Callum Connolly <callumc11@gmail.com>
2020-04-12 21:21:31 +0100

    Added files for CWE193 and according Exploit Test

---
+++ src/main/java/cc14g17/CWE193_Off_by_One_Error.java
@@ -0,0 +1,17 @@
+/*
+* @description
+* CWE: 193
+* An array index is 1 off from the max array index
+*
+ */
+
+package cc14g17;
+
+public class CWE193_Off_by_One_Error {
+
+    private final String CLASSNAME = "CWE193_Off_by_One_Error";
+
+    public void runTest(String className) {
+
+    }
+}
---
+++ src/test/java/cc14g17/CWE193_Off_by_One_ErrorTest.java
@@ -0,0 +1,10 @@
+package cc14g17;
+
+import org.junit.Test;
+
+public class CWE193_Off_by_One_ErrorTest {
+
+    @Test
+    public void runTest() {
+    }
+}
